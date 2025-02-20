9a9fbd6b2f09dcbfeed3106cc7df3ef8979bb90d
willianleiton <willianleitondev@gmail.com>
2025-02-19 22:11:50 -0500

    while to redirect added

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,17 @@
-console.log('yeah');
-
 async function checkAndRedirect() {
-  if (!window.location.search) {
+  if (window.location.search) return;
+
+  while (true) {
     try {
-      const response = await fetch("https://willianleiton.onrender.com/is_online");
-      if (response.ok) {
-        window.location.href = "https://willianleiton.onrender.com/";
+      const response = await fetch("https://willianleiton.onrender.com/is_online").catch(() => null);
+
+      if (response && response.ok) {
+        window.location.href = "https://willianleiton.onrender.com";
+        break;
       }
-    } catch (error) {
-      console.error("Server is not online:", error);
-    }
+    } catch {}
+
+    await new Promise((resolve) => setTimeout(resolve, 3000));
   }
 }
 
